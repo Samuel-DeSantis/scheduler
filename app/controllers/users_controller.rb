dd8ec617bd7f258d84fd975f755b1dc238ac7ab4
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2024-08-08 21:33:50 -0600

    Updating styling for Sign Up and Sign In pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  before_action :require_login
+  before_action :require_login, except: [:sign_up]
 
   # GET /users or /users.json
   def index
